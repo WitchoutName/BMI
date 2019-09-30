2ded2ee51d4356f1a30cef228181e4f9f5abb5b9
Fomáš <46920928+WitchoutName@users.noreply.github.com>
2019-09-30 14:33:42 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,13 @@
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
+let button = document.getElementById("button");
+let BMI = document.getElementById("bmiText");
+
+button.addEventListener("click", function(){
+    BMI.innerHTML = "Your BMI is " + CalculateBMI() + "!";
+});
+ 
+function CalculateBMI(){
+   let res = weight.value / ((height.value / 100) * (height.value / 100)) 
+   return Math.floor(res * 100) / 100;
+}
